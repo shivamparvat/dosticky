a5bb9b293ababb800dd14a41b9d0ca411124e54f
Yadav7789~ <yadavji7789@gmail.com>
2023-02-04 17:33:35 +0530

    cludinare

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ const database = require('./config/database');
 const cloudinary = require('cloudinary');
 
 dotenv.config({ path: "./config/config.env" });
-cloudinary.config({ 
+
+
+cloudinary.v2.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_KEY, 
     api_secret: process.env.CLOUDINARY_SECRET,
     secure: true
   });
 
+
 // data base conection
 database();
+
 
 // servar lition
 app.listen(process.env.PORT, () => console.log(`server started http://localhost:${process.env.PORT}`));
---
+++ server/utils/dataUri.js
@@ -0,0 +1,1 @@
+const DataUri = require("dataurl")
