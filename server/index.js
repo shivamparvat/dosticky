92bfc038b428cd5ade8356b9e4f57360fc124ce8
Yadav7789~ <yadavji7789@gmail.com>
2023-02-04 10:13:24 +0530

    config cloudinary

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const app = require('./app')
 const dotenv = require('dotenv');
 const database = require('./config/database');
+const cloudinary = require('cloudinary');
 
 dotenv.config({ path: "./config/config.env" });
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET,
+    secure: true
+  });
 
 // data base conection
 database();
